839aae8cbe286b6b280119663f25f74b238bd6b9
vaibhav <goyal.vaibhav10@gmail.com>
2022-05-13 15:21:14 +0530

    random

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ while user_input != "exit":
     days_and_unit_dictionary = {"Days": days_and_unit[0], "Units": days_and_unit[1]}
     print(days_and_unit_dictionary)
     validate_and_execute(days_and_unit_dictionary)
-# Created a nw branch
+
